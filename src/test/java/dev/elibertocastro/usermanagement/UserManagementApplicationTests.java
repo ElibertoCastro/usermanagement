85cd72667fb5ed58a069028d94671641a732cd7f
Eliberto Silva Castro <elibertocastro@gmai.com>
2025-03-10 10:49:00 -0300

    First commit

---
+++ src/test/java/dev/elibertocastro/usermanagement/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elibertocastro.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
